8bbb4fe2f03289162b58e8b73665037e986d1491
Yusuf Modan <yumodan@gmail.com>
2016-04-13 00:03:53 -0700

    make function names more descriptive

--- binaryHeap.js
+++ binaryHeap.js
@@ -9,12 +9,12 @@ BinaryHeap.prototype.getRoot = function () {
 }
 BinaryHeap.prototype.insert = function (value) {
   this._heap.push(value);
-  this.bubbleUp(this.parent(this._heap.length - 1));
+  this.bubble(this.parent(this._heap.length - 1));
 }
 BinaryHeap.prototype.removeRoot = function () {
   swap(this._heap.length - 1, 0, this._heap);
   let temp = this._heap.pop()
-  this.bubbleUp(0, false); //this will actually bubble down bc of the second param
+  this.bubble(0, false); //this will actually bubble down bc of the second param
   return temp;
 }
 
@@ -26,7 +26,7 @@ BinaryHeap.prototype.isOutOfPlace = function(rightIndex, leftIndex, index) {
   return this._heap[rightIndex] < this._heap[index] || this._heap[leftIndex] < this._heap[index];
 }
 
-BinaryHeap.prototype.bubbleUp = function(index, up){
+BinaryHeap.prototype.bubble = function(index, up){ //up must be false (bool) to bubble down
   //if you want to bubble up index must be the parent node
   //second parameter is bool. true to bubble up false to bubble down
   let rightIndex = this.rightChild(index);
@@ -37,19 +37,19 @@ BinaryHeap.prototype.bubbleUp = function(index, up){
   //variable is checks the node at index index is greater than its child nodes
   //if it is greater than its child nodes
     //swap with smallest child node
-    //if you want to bubble down call the bubbleUP function recursively on the index of the smaller child node
-    //if you want to bubble up call the bubbleUp  function recursively on the parent node
+    //if you want to bubble down call the bubble function recursively on the index of the smaller child node
+    //if you want to bubble up call the bubble  function recursively on the parent node
 
   if(is){
     if (this._heap[leftIndex] < this._heap[rightIndex]){ //if this._heap[leftIndex or rightIndex] is undefined it will fall into the else statement
       swap(index, leftIndex, this._heap);
       up === false ? i = this.leftChild(index) : i = this.parent(index)
-      this.bubbleUp(i);
+      this.bubble(i, up);
     }else{
       if(this._heap[rightIndex]){ //make sure we are not accessing null values
         swap(index, rightIndex, this._heap);
         up === false ? i = this.rightChild(index) : i = this.parent(index);
-        this.bubbleUp(i);
+        this.bubble(i, up);
       }
     }
   }
